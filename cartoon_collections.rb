eef91121c80b7b2094c68cab3beae150815d5b23
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-16 01:42:17 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,17 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |name|
+    puts "Hello #{name}!"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |name, index|
+    index = index + 1 # Is there a better way than this to start a list of array indices from 1 instead of 0?
+    print "#{index}. #{name} "
+  end
 end
