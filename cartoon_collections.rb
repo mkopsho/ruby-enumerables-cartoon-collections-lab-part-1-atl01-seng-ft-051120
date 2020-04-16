d4e03ed00d2d8a8f2764a049a26658268e62d9aa
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-16 01:42:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |name, index|
-    index = index + 1 # Is there a better way to start a list of array indices from 1 instead of 0?
+    index = index + 1 # Is there a better way than this to start a list of array indices from 1 instead of 0?
     print "#{index}. #{name} "
   end
 end
